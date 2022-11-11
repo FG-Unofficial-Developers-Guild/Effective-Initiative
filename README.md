65421beefb2b71fc33c0580edb9ad2531cc53cfc
Ryan Hagelstrom <rhagelstrom@gmail.com>
2022-11-10 21:05:34 -0600

    Feature - 5E only: Added option to have requested rolls ask for rolls each round

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
 # Effective-Initiative
+
+**Current Version:** 1.1
+**Last Updated:** 11/10/22
+
 Extension for FantasyGrounds which recalculates effects "initiative to act on" value at the start of each round when using the house rule "Roll init each round"
+
+| Name| Default | Options | Notes |
+|---|---|---|---|
+|CT: Leverage Requested Rolls| Off| Off,Only PCs,All| 5E Only: Requested Rolls will ask for initiative roll each round for PCs & Friend Zone Cohorts, or all on CT|
--- scripts/manager_effect_effective_initiative.lua
+++ scripts/manager_effect_effective_initiative.lua
@@ -2,28 +2,104 @@
 --	  	Copyright © 2021
 --	  	This work is licensed under a Creative Commons Attribution-ShareAlike 4.0 International License.
 --	  	https://creativecommons.org/licenses/by-sa/4.0/
+local handleApplyInit = nil
+local onTurnStartEvent = nil
+local nextActor = nil
+
+-- Counter of outstanding init rolls
+local nOutstandingRolls = 0
 
 function customRoundStart()
 	if OptionsManager.isOption("HRIR", "on")  then
-		local ctEntries = CombatManager.getSortedCombatantList()
-		for _, nodeCT in pairs(ctEntries) do
-			for _,nodeEffect in pairs(DB.getChildren(nodeCT, "effects")) do
-				if (DB.getValue(nodeEffect, "duration", "") ~= 0) then
-					local sSource = DB.getValue(nodeEffect, "source_name", "")
-					if sSource == "" then
-						sSource	= ActorManager.getCTPathFromActorNode(nodeCT)
-					end
-					local nodeSource = ActorManager.getCTNode(sSource)
-					local nInit = DB.getValue(nodeSource, "initresult", 0)
-					DB.setValue(nodeEffect, "init", "number", nInit)
+		if User.getRulesetName() == "5E" and RRRollManager and not OptionsManager.isOption("EFFECTIVE_INITIATIVE", "off") then
+			nOutstandingRolls = 0
+			local ctEntries = CombatManager.getCombatantNodes()
+			for _, nodeCT in pairs(ctEntries) do
+				local bCohort = false
+				if FriendZone then
+					bCohort = FriendZone.isCohort(nodeCT)
+				end
+				if OptionsManager.isOption("EFFECTIVE_INITIATIVE", "all") or (OptionsManager.isOption("EFFECTIVE_INITIATIVE", "pcs") and (ActorManager.isPC(nodeCT) or bCohort)) then
+					nOutstandingRolls = nOutstandingRolls + 1
+					RRRollManager.onButtonPress("init",nodeCT)
 				end
 			end
+			if nOutstandingRolls == 0 then
+				reevaluateAllEffects()
+			end
+		else
+			reevaluateAllEffects()
 		end
+	end
+end
+
+function reevaluateAllEffects()
+	local ctEntries = CombatManager.getCombatantNodes()
+	for _, nodeCT in pairs(ctEntries) do
+		reevaluateEffects(nodeCT)
+	end
+end
+
+function reevaluateEffects(nodeCT)
+	for _,nodeEffect in pairs(DB.getChildren(nodeCT, "effects")) do
+		if (DB.getValue(nodeEffect, "duration", "") ~= 0) then
+			local sSource = DB.getValue(nodeEffect, "source_name", "")
+			if sSource == "" then
+				sSource	= ActorManager.getCTPathFromActorNode(nodeCT)
+			end
+			local nodeSource = ActorManager.getCTNode(sSource)
+			local nInit = DB.getValue(nodeSource, "initresult", 0)
+			DB.setValue(nodeEffect, "init", "number", nInit)
+		end
+	end
+end
+
+function customHandleApplyInit(msgOOB)
+	handleApplyInit(msgOOB)
+	if nOutstandingRolls > 0 then
+		nOutstandingRolls = nOutstandingRolls - 1
+		--Wait until everything is settled before readjusting the order of the effects
+		if nOutstandingRolls == 0 then
+			reevaluateAllEffects()
+			nextActor()
+		end
+	end
+end
+
+function customOnTurnStartEvent(nodeCT)
+	if nOutstandingRolls == 0 then
+		onTurnStartEvent(nodeCT)
+	end
+end
+
+function customNextActor(SkipBell, bNoRoundAdvance)
+	if nOutstandingRolls == 0 then
+		nextActor(SkipBell, bNoRoundAdvance);
 	end
 end
 
 function onInit()
 	if Session.IsHost then
 		CombatManager.setCustomRoundStart(customRoundStart)
+		if  User.getRulesetName() == "5E" then
+			OOBManager.registerOOBMsgHandler(ActionInit.OOB_MSGTYPE_APPLYINIT, customHandleApplyInit)
+
+			handleApplyInit = ActionInit.handleApplyInit
+			nextActor = CombatManager.nextActor
+			onTurnStartEvent = CombatManager.onTurnStartEvent
+			ActionInit.handleApplyInit = customHandleApplyInit
+			CombatManager.onTurnStartEvent = customOnTurnStartEvent
+			CombatManager.nextActor = customNextActor
+
+			OptionsManager.registerOption2("EFFECTIVE_INITIATIVE", false, "option_effective_initiative",
+			"option_ei_rr", "option_entry_cycler",
+			{
+				labels = "PCs Only|All",
+				values = "pcs|all",
+				baselabel = "option_val_off",
+				baseval = "off",
+				default = "off"
+			});
+		end
 	end
 end
